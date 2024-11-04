99286380df986941fe1478fc8124bf569dc83a1d
tzh <tzh_lg@163.com>
2024-11-04 16:17:34 +0800

    1

---
+++ m7yx/index.js
@@ -0,0 +1,22 @@
+﻿var conf = {
+    v: "1.0.0.0",
+    maintaining: false,
+    index: ""
+}
+
+function go(v, prefix) {
+    window.location.href = getUrl(v, prefix)
+}
+
+function getUrl(v, prefix) {
+    if (conf.maintaining) {
+        return prefix + "pages/maintaining.html";
+    }
+
+    if (v != conf.v) {
+        return prefix + "pages/update.html";
+    }
+
+    //return conf.index;
+    return prefix + "pages/index.html";
+}
